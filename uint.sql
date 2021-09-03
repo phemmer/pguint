57bd1e5800530adfd058d29ae911af1020c9dd66
Patrick Hemmer <phemmer@haproxy.com>
2021-09-03 07:40:17 -0400

    mark functions as parallel safe

--- uint.sql
+++ uint.sql
@@ -3,24 +3,28 @@ CREATE TYPE int1;
 CREATE FUNCTION int1in(cstring) RETURNS int1
     IMMUTABLE
     STRICT
+    PARALLEL SAFE
     LANGUAGE C
     AS '$libdir/uint', 'int1in';
 
 CREATE FUNCTION int1out(int1) RETURNS cstring
     IMMUTABLE
     STRICT
+    PARALLEL SAFE
     LANGUAGE C
     AS '$libdir/uint', 'int1out';
 
 CREATE FUNCTION int1recv(internal) RETURNS int1
     IMMUTABLE
     STRICT
+    PARALLEL SAFE
     LANGUAGE C
     AS '$libdir/uint', 'int1recv';
 
 CREATE FUNCTION int1send(int1) RETURNS bytea
     IMMUTABLE
     STRICT
+    PARALLEL SAFE
     LANGUAGE C
     AS '$libdir/uint', 'int1send';
 
@@ -48,24 +52,28 @@ CREATE TYPE uint1;
 CREATE FUNCTION uint1in(cstring) RETURNS uint1
     IMMUTABLE
     STRICT
+    PARALLEL SAFE
     LANGUAGE C
     AS '$libdir/uint', 'uint1in';
 
 CREATE FUNCTION uint1out(uint1) RETURNS cstring
     IMMUTABLE
     STRICT
+    PARALLEL SAFE
     LANGUAGE C
     AS '$libdir/uint', 'uint1out';
 
 CREATE FUNCTION uint1recv(internal) RETURNS uint1
     IMMUTABLE
     STRICT
+    PARALLEL SAFE
     LANGUAGE C
     AS '$libdir/uint', 'uint1recv';
 
 CREATE FUNCTION uint1send(uint1) RETURNS bytea
     IMMUTABLE
     STRICT
+    PARALLEL SAFE
     LANGUAGE C
     AS '$libdir/uint', 'uint1send';
 
@@ -93,24 +101,28 @@ CREATE TYPE uint2;
 CREATE FUNCTION uint2in(cstring) RETURNS uint2
     IMMUTABLE
     STRICT
+    PARALLEL SAFE
     LANGUAGE C
     AS '$libdir/uint', 'uint2in';
 
 CREATE FUNCTION uint2out(uint2) RETURNS cstring
     IMMUTABLE
     STRICT
+    PARALLEL SAFE
     LANGUAGE C
     AS '$libdir/uint', 'uint2out';
 
 CREATE FUNCTION uint2recv(internal) RETURNS uint2
     IMMUTABLE
     STRICT
+    PARALLEL SAFE
     LANGUAGE C
     AS '$libdir/uint', 'uint2recv';
 
 CREATE FUNCTION uint2send(uint2) RETURNS bytea
     IMMUTABLE
     STRICT
+    PARALLEL SAFE
     LANGUAGE C
     AS '$libdir/uint', 'uint2send';
 
@@ -138,24 +150,28 @@ CREATE TYPE uint4;
 CREATE FUNCTION uint4in(cstring) RETURNS uint4
     IMMUTABLE
     STRICT
+    PARALLEL SAFE
     LANGUAGE C
     AS '$libdir/uint', 'uint4in';
 
 CREATE FUNCTION uint4out(uint4) RETURNS cstring
     IMMUTABLE
     STRICT
+    PARALLEL SAFE
     LANGUAGE C
     AS '$libdir/uint', 'uint4out';
 
 CREATE FUNCTION uint4recv(internal) RETURNS uint4
     IMMUTABLE
     STRICT
+    PARALLEL SAFE
     LANGUAGE C
     AS '$libdir/uint', 'uint4recv';
 
 CREATE FUNCTION uint4send(uint4) RETURNS bytea
     IMMUTABLE
     STRICT
+    PARALLEL SAFE
     LANGUAGE C
     AS '$libdir/uint', 'uint4send';
 
@@ -183,24 +199,28 @@ CREATE TYPE uint8;
 CREATE FUNCTION uint8in(cstring) RETURNS uint8
     IMMUTABLE
     STRICT
+    PARALLEL SAFE
     LANGUAGE C
     AS '$libdir/uint', 'uint8in';
 
 CREATE FUNCTION uint8out(uint8) RETURNS cstring
     IMMUTABLE
     STRICT
+    PARALLEL SAFE
     LANGUAGE C
     AS '$libdir/uint', 'uint8out';
 
 CREATE FUNCTION uint8recv(internal) RETURNS uint8
     IMMUTABLE
     STRICT
+    PARALLEL SAFE
     LANGUAGE C
     AS '$libdir/uint', 'uint8recv';
 
 CREATE FUNCTION uint8send(uint8) RETURNS bytea
     IMMUTABLE
     STRICT
+    PARALLEL SAFE
     LANGUAGE C
     AS '$libdir/uint', 'uint8send';
 
@@ -223,7 +243,7 @@ CREATE CAST (uint8 AS numeric) WITH INOUT AS IMPLICIT;
 CREATE CAST (uint8 AS real) WITH INOUT AS IMPLICIT;
 
 
-CREATE FUNCTION int1um(int1) RETURNS int1 IMMUTABLE STRICT LANGUAGE C AS '$libdir/uint', 'int1um';
+CREATE FUNCTION int1um(int1) RETURNS int1 IMMUTABLE STRICT PARALLEL SAFE LANGUAGE C AS '$libdir/uint', 'int1um';
 
 CREATE OPERATOR - (
    PROCEDURE = int1um,
